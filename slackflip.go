a1257f9c56b737187957918d4d3956fe24e0bcaf
John Barton (joho) <jrbarton@gmail.com>
2016-03-18 15:19:20 +1100

    Initial commit.

---
+++ slackflip.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	portNumber := os.Getenv("PORT")
+	if portNumber == "" {
+		portNumber = "5000"
+	}
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+
+		fmt.Fprintf(w, "(╯°□°）╯︵ ┻━┻")
+	}
+
+	http.HandleFunc("/", handler)
+
+	log.Printf("(╯°□°）╯︵ ┻━┻ on port %v", portNumber)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", portNumber), nil))
+}
